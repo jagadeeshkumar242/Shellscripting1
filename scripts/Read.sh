74ecaa04607798595d3b976ad04b90a2a6526eca
jagadeesh <jagadeeshkumar242@gmail.com>
2023-11-04 11:15:44 +0530

    commiting scripting files

---
+++ scripts/Read.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# reads the input from username
+
+#echo "Enter Username: "
+#read username
+#echo $username
+
+read -p "Enter the username: " username
+echo $username
+
+read -sp "Enter password: " password
+echo ""
+echo $password
---
+++ scripts/arguments_passing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Display the filename script itself
+
+echo "Filename Argument: "$0
+
+# Display the first argument
+
+echo "First Argument: "$1
+
+# Display the second argument
+
+echo "Second Argument: "$2
+
+# Display the total arguments
+
+echo "Total arguments: "$@
+
+#Display the number of arguments
+
+echo "No of arguments: "$#
